ecf1e4d159b768618113773c951913b3d253b459
Rafael <rafaelmarliere033@gmail.com>
2023-04-01 16:32:40 -0300

    Resolução dos 20 exercícios de Array e Slice.

--- main.go
+++ main.go
@@ -231,4 +231,214 @@ func main() {
 	fmt.Printf("Maior valor: %d.\n", maior)
 	fmt.Printf("Menor valor: %d.", menor)
 
+	// 11. Crie um Array bidimensional de inteiros com 2 linhas e 3 colunas. Em seguida, solicite ao usuário que informe um índice de linha e outro de coluna e imprima o valor armazenado nessa posição da matriz.
+
+	matriz := [2][3]int{}
+
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			var valor int
+			fmt.Printf("Digite o valor da posição [%d][%d]: ", i, j)
+			fmt.Scan(&valor)
+			matriz[i][j] = valor
+
+			fmt.Println("Matriz resultante:")
+			for i := 0; i < 2; i++ {
+				for j := 0; j < 3; j++ {
+					fmt.Printf("%d ", matriz[i][j])
+				}
+				fmt.Println()
+			}
+		}
+	}
+
+	// 12. Crie um Array de inteiros com 5 elementos. Em seguida, crie um novo Slice que contenha apenas os elementos do Array que são múltiplos de 3. Imprima o novo Slice.
+
+	array := []int{1, 2, 3, 4, 5}
+	slice := []int{}
+
+	for _, i := range array {
+		if i%3 == 0 {
+			slice = append(slice, i)
+		}
+	}
+
+	fmt.Println(slice)
+
+	// 13. Crie um Array de inteiros com 7 elementos. Solicite ao usuário que informe um número que será adicionado ao primeiro e ao último elemento do Array. Imprima o Array resultante.
+
+	array := []int{1, 2, 3, 4, 5, 6, 7}
+	var x int
+	var y int
+
+	fmt.Print("Usuário, digite um número que queira adicionar ao início da lista, por favor: ")
+	fmt.Scan(&x)
+	fmt.Print("Usuário, digite um número que queira adicionar ao final da lista, por favor: ")
+	fmt.Scan(&y)
+
+	array = append([]int{x}, array...)
+	array = append(array, y)
+	fmt.Print(array)
+
+	// 14. Crie um Slice de inteiros com tamanho 8 e solicite ao usuário que informe dois índices de elementos que deverão ser trocados de posição. Imprima o Slice resultante.
+
+	slice := make([]int, 8)
+
+	for i := 0; i < len(slice); i++ {
+		fmt.Print("Digite o valor do elemento: ")
+		fmt.Scan(&slice[i])
+	}
+
+	var i, j int
+	fmt.Print("Digite o elemento que deseja trocar: ")
+	fmt.Scan(&i)
+	fmt.Print("Digite o elemento que quer usar para fazer a troca com o de cima: ")
+	fmt.Scan(&j)
+
+	slice[i], slice[j] = slice[j], slice[i]
+
+	fmt.Println(slice)
+
+	// 15. Crie um Array de floats com 10 elementos. Crie um novo Slice que contenha apenas os elementos do Array que são maiores que 5. Imprima o novo Slice.
+
+	array := make([]float64, 10)
+	slice := []float64{}
+
+	for i := 0; i < len(array); i++ {
+		fmt.Print("Digite o valor do elemento: ")
+		fmt.Scan(&array[i])
+	}
+
+	for _, valor := range array {
+		if valor > 5 {
+			slice = append(slice, valor)
+		}
+	}
+
+	fmt.Println(slice)
+
+	// 16. Crie um Array de inteiros com 10 elementos. Crie um novo Slice que contenha apenas os elementos pares do Array original. Imprima o novo Slice.
+
+	array := make([]int, 10)
+	slice := []int{}
+
+	for i := 0; i < len(array); i++ {
+		fmt.Print("Digite o valor de um elemento, por favor: ")
+		fmt.Scan(&array[i])
+	}
+
+	for _, valor := range array {
+		if valor%2 == 0 {
+			slice = append(slice, valor)
+		}
+	}
+
+	fmt.Print(slice)
+
+	//. 17 Crie um Array de inteiros com 10 elementos. Calcule e imprima a soma dos elementos nas posições pares do Array.
+
+	arr := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	sum := 0
+	for i := 0; i < len(arr); i += 2 {
+		sum += arr[i]
+	}
+	fmt.Println("A soma dos elementos nas posições pares do Array é:", sum)
+
+	// 18. Escreva um programa que leia um número inteiro positivo n e gere um array com os n primeiros números primos.
+
+	package main
+
+	import "fmt"
+
+	func isPrimo(n int) bool {
+		if n < 2 {
+		return false
+	}
+
+		for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+		return false
+	}
+	}
+
+		return true
+	}
+
+	func main() {
+		var n int
+
+		fmt.Print("Digite um número inteiro positivo: ")
+		fmt.Scan(&n)
+
+		primos := make([]int, 0, n)
+
+		i := 2
+		for len(primos) < n {
+			if isPrimo(i) {
+				primos = append(primos, i)
+			}
+			i++
+		}
+
+		fmt.Println(primos)
+	}
+
+	// Obs: Foi necessário a abertura de outra função, logo, para verificar o código, vai ter que copiar e colar.
+
+	// 19. Faça um programa que leia dois arrays de inteiros de tamanho n e gere um terceiro array que seja a soma dos dois primeiros.
+
+	var t int
+	fmt.Print("Digite o tamanho dos arrays: ")
+	fmt.Scan(&t)
+
+	array1 := make([]int, t)
+	array2 := make([]int, t)
+
+	fmt.Println("Digite os valores do primeiro array:")
+	for i := 0; i < t; i++ {
+		fmt.Printf("Digite o valor %d: ", i + 1)
+		fmt.Scan(&array1[i])
+	}
+
+	fmt.Println("Digite os valores do segundo array:")
+	for i := 0; i < t; i++ {
+		fmt.Printf("Digite o valor %d: ", i + 1)
+		fmt.Scan(&array2[i])
+	}
+
+	array3 := make([]int, t)
+	for i := 0; i < t; i++ {
+		array3[i] = array1[i] + array2[i]
+	}
+
+	fmt.Println("A soma dos dois arrays é:", array3)
+
+	// 20. Crie um programa que leia um array de inteiros e verifique se ele está ordenado em ordem crescente.
+
+		var t int
+		fmt.Print("Informe o tamanho do array: ")
+		fmt.Scan(&t)
+
+		arr := make([]int, t)
+		fmt.Printf("Digite %d números inteiros separados por espaço: ", t)
+		for i := 0; i < t; i++ {
+			fmt.Scan(&arr[i])
+		}
+
+		crescente := true
+		for i := 1; i < t; i++ {
+			if arr[i] < arr[i - 1] {
+				crescente = false
+				break
+			}
+		}
+
+		if crescente {
+			fmt.Println("O array está em ordem crescente.")
+		} else {
+			fmt.Println("O array não está em ordem crescente.")
+		}
+
+		// Professor, está entregue, mas, sendo sincero com o senhor, fiquei com dúvida em vários, alguns eu pedi no ChatGPT e pedi para ele me explicar, outros exercícios eu consegui desenrolar e deram certo. Mas fui aprendendo, utilizando e pesquisando. Inclusive acho que esse é o intúito dos exercícios.
+
 }
